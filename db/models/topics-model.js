65bd3a233a265c8f6252c4b042dc2f224aeac658
Zak Saleh <zaksaleh@MacBook-Pro-2.local>
2023-03-28 10:43:22 +0100

    unused code deleted

--- db/models/topics-model.js
+++ db/models/topics-model.js
@@ -5,11 +5,3 @@ exports.fetchAllTopics = () => {
     return result.rows;
   });
 };
-
-exports.fetchArticleId = (article_id) => {
-  return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-    .then((result) => {
-      return result.rows;
-    });
-};
