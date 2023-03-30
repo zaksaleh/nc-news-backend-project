e53e20ecbef0134a500bc8a04b690fee4af852ad
Zak Saleh <zaksaleh@MacBook-Pro-2.local>
2023-03-30 15:37:42 +0100

    9-DELETE-ncnews9

--- db/models/comments-model.js
+++ db/models/comments-model.js
@@ -35,3 +35,15 @@ exports.insertComment = (article_id, username, body) => {
       return result.rows;
     });
 };
+
+exports.removeComment = (comment_id) => {
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [
+      comment_id,
+    ])
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return Promise.reject({ status: 404, msg: "ID not found" });
+      }
+    });
+};
